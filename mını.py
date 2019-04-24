afc8ed8ee1301c2eb33093c3b902ef82739d8907
hibedeger <48417253+hibedeger@users.noreply.github.com>
2019-04-24 14:45:21 +0300

    Add files via upload

---
+++ mını.py
@@ -0,0 +1,19 @@
+metin ="Açık bilim, araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini, bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+print(metin[0:20])
+liste = ['Açık Bilim', 'Açık Erişim', 'Açık Lisans', 'Açık Eğitim', 'Açık Veri', 'Açık Kültür']
+print(liste)
+for i in liste:
+    print(i)
+    cevap=input(kelıme gir)
+sözluk={'Elma' : 'Ağaçta yetişen bir tür meyve', 'Salatalık' : 'Fidan üzerinde büyüyen bir tür sebze'}
+print (sözluk)
+      if cevap =="elma":
+          print("Ağaçta yetişen bir tür meyve")
+if cevap=="salatalık":
+    print("Fidan üzerinde büyüyen bir tür sebze")
+else
+print("tebrikler")
+
+
+
+
